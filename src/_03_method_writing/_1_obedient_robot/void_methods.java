e7cf4d4f7dec046442f8e1724e3f888dfc69942d
root <root@c9dc9a8ea228>
2022-03-16 01:34:02 +0000

    IntroToMethods and void_methods

---
+++ src/_03_method_writing/_1_obedient_robot/void_methods.java
@@ -0,0 +1,20 @@
+package _03_method_writing._1_obedient_robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class void_methods {
+	public static void main(String[] args) {
+		Robot bob = new Robot();
+		bob.setX(60);
+		bob.penDown();
+		bob.setSpeed(5);
+	public static void drawSquare() {
+		int x = 0;
+		while(x<4)
+			bob.move(30);
+			bob.turn(90);
+		
+		
+	}
+	}
+}
