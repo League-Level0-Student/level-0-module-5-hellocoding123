ea14542784546e9b86cee0b10da30af4806779b8
student <student@ip-172-31-48-53.us-west-2.compute.internal>
2022-03-22 18:32:06 -0700

    method_writing

--- src/_03_method_writing/_1_obedient_robot/void_methods.java
+++ src/_03_method_writing/_1_obedient_robot/void_methods.java
@@ -8,13 +8,17 @@ public class void_methods {
 		bob.setX(60);
 		bob.penDown();
 		bob.setSpeed(5);
+		drawSquare();
+	}
 	public static void drawSquare() {
 		int x = 0;
-		while(x<4)
+		while(x<4) {
 			bob.move(30);
 			bob.turn(90);
+			x++;
 		
+		}
 		
 	}
-	}
+	
 }
