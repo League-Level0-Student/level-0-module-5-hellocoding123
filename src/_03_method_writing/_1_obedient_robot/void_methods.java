efd3a27a31e67deaba5acf76864dcf114b836649
root <root@7c40e86424a4>
2022-03-30 01:31:07 +0000

    Method_writing

--- src/_03_method_writing/_1_obedient_robot/void_methods.java
+++ src/_03_method_writing/_1_obedient_robot/void_methods.java
@@ -3,15 +3,27 @@ package _03_method_writing._1_obedient_robot;
 import org.jointheleague.graphical.robot.Robot;
 
 public class void_methods {
+	
+	
 	public static void main(String[] args) {
 		Robot bob = new Robot();
+		int x = 0;
 		bob.setX(60);
 		bob.penDown();
 		bob.setSpeed(5);
-		drawSquare();
+		while(x<4) {
+			bob.move(30);
+			bob.turn(90);
+			x++;
+		
+		}
+	
 	}
-	public static void drawSquare() {
+	public void drawSquare() {
 		int x = 0;
+		bob.setX(60);
+		bob.penDown();
+		bob.setSpeed(5);
 		while(x<4) {
 			bob.move(30);
 			bob.turn(90);
