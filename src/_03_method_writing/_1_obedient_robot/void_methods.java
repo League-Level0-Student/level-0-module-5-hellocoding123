8e56b2976b33a54c195e49d23c7b61c994822d1e
root <root@c7255e98a6f9>
2022-04-06 01:29:53 +0000

    method_writing_2

--- src/_03_method_writing/_1_obedient_robot/void_methods.java
+++ src/_03_method_writing/_1_obedient_robot/void_methods.java
@@ -8,29 +8,39 @@ public class void_methods {
 	public static void main(String[] args) {
 		Robot bob = new Robot();
 		int x = 0;
+		bob.setSpeed(5);
 		bob.setX(60);
 		bob.penDown();
+		bob.setPenWidth(5);
 		bob.setSpeed(5);
 		while(x<4) {
-			bob.move(30);
+			bob.move(200);
 			bob.turn(90);
 			x++;
-		
 		}
-	
-	}
-	public void drawSquare() {
-		int x = 0;
-		bob.setX(60);
+		x = 0;
+		bob.penUp();
+		bob.setX(300);
 		bob.penDown();
-		bob.setSpeed(5);
-		while(x<4) {
-			bob.move(30);
-			bob.turn(90);
+		bob.turn(120);
+		bob.move(200);
+		bob.turn(120);
+		bob.move(200);
+		bob.turn(120);
+		bob.move(200);
+		
+		bob.penUp();
+		bob.setX(550);
+		bob.penDown();
+		bob.setSpeed(10);
+		while(x<360) {
+			bob.move(2);
+			bob.turn(1);
 			x++;
-		
 		}
-		
 	}
+  }
 	
-}
+		
+	
+	
