8e3e88f8b350eb1294302a671318ad9b953b4826
root <root@6b598e76d789>
2022-04-13 01:29:01 +0000

    method_writing_3

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -14,7 +14,7 @@ public class HappyPet {
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			int task = JOptionPane.showOptionDialog(null, "Which pet do you want?", "Choose your pet!", 0, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "Button1", "Button2", "Button3" }, null);
 
 			// 6. Use user input to call the appropriate method created in step 5 below.
